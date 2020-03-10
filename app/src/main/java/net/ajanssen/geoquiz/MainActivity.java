3e75829329b4695ea8bd6c07c69788ba1b5a02fb
AlexJanss <alex.janssen20@gmail.com>
2020-03-10 09:29:27 +0100

    add buttons to mainactivity

--- app/src/main/java/net/ajanssen/geoquiz/MainActivity.java
+++ app/src/main/java/net/ajanssen/geoquiz/MainActivity.java
@@ -3,8 +3,12 @@ package net.ajanssen.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
