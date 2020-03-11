3790f4ef3bf2d90563d2c851cee33ae00c460b99
AlexJanss <alex.janssen20@gmail.com>
2020-03-11 09:11:46 +0100

    generate getter and setter for variables

--- app/src/main/java/net/ajanssen/geoquiz/TrueFalse.java
+++ app/src/main/java/net/ajanssen/geoquiz/TrueFalse.java
@@ -9,4 +9,20 @@ public class TrueFalse extends Object {
         mQuestion = question;
         mTrueQuestion = trueQuestion;
     }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
 }
